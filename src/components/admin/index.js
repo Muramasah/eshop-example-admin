13ff5a7ae7644f679b223907310ef626c02f182f
Felipe Medina <medina.xavier.felipe@gmail.com>
2018-09-29 22:49:22 -0300

    Modified create-react-app boilerplate

---
+++ src/components/admin/index.js
@@ -0,0 +1,18 @@
+// Libraries
+import React from 'react';
+// Components
+import Header from '../header';
+import './index.css';
+
+const Admin = () => {
+  return (
+    <div className="App">
+      <Header/>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+}
+
+export default Admin;
